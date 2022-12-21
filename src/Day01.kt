413c2151e32d131136113831428c3c1d9f20a6a5
minhthong0411 <minhthong0411@yahoo.com>
2022-12-21 00:56:32 -0800

    Day 01 Code Completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
+import java.util.SortedSet
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var carriedMost = 0
+        var elfTotal = 0
+        input.forEach { str ->
+            if(str.isEmpty()){
+                if(elfTotal > carriedMost){
+                    carriedMost = elfTotal
+                }
+                elfTotal = 0
+            }else{
+                elfTotal += str.toInt()
+            }
+        }
+        return carriedMost
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sortedSet: SortedSet<Int> = sortedSetOf()
+        var elfTotal = 0
+        input.forEach { str ->
+            if(str.isEmpty()){
+                sortedSet.add(elfTotal)
+                elfTotal = 0
+            }else{
+                elfTotal += str.toInt()
+            }
+        }
+        sortedSet.add(elfTotal)
+        return sortedSet.reversed().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
