fe12c5e315c55d6e43f58d3d53ca794628498e21
minhthong0411 <minhthong0411@yahoo.com>
2023-01-02 02:19:38 -0800

    Day 10 Code Completed

---
+++ src/Day10.kt
@@ -0,0 +1,93 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var currentCycle = 0
+        var xVal = 1
+        var signalStrength = 0
+        input.forEach { command ->
+           //println("READ: $command")
+            if(command.contains("noop")){
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                //println("During Noop cyle curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+            }else{
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                //println("During ADDX first Cylce curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                xVal += command.split(' ')[1].toInt()
+                //println("During ADDX sceond Cylce curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+            }
+        }
+        return signalStrength
+    }
+
+
+    fun part2(input: List<String>): Int {
+        var currentCycle = 0
+        var xVal = 1
+        var signalStrength = 0
+        var crt = ""
+        var longString = ""
+        input.forEach { command ->
+            println("READ: $command")
+            if(command.contains("noop")){
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                if(crt.length in xVal - 1..xVal + 1) crt += "#" else crt += "."
+                if(crt.length % 40 == 0) {
+                    longString += crt
+                    longString += "\n"
+                    crt = ""
+                }
+                println("During Noop cyle curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+                println(longString)
+            }else{
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                if(crt.length in xVal - 1..xVal + 1) crt += "#" else crt += "."
+                if(crt.length % 40 == 0) {
+                    longString += crt
+                    longString += "\n"
+                    crt = ""
+                }
+
+                println("During ADDX first Cylce curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+                println(longString)
+                currentCycle += 1
+                if(currentCycle % 40 == 20){
+                    signalStrength += (xVal * currentCycle)
+                }
+                if(crt.length in xVal - 1..xVal + 1) crt += "#" else crt += "."
+                if(crt.length % 40 == 0) {
+                    longString += crt
+                    longString += "\n"
+                    crt = ""
+                }
+                xVal += command.split(' ')[1].toInt()
+                println("During ADDX sceond Cylce curr X: $xVal currCycle: $currentCycle signal: $signalStrength")
+                println(longString)
+            }
+        }
+        println(crt)
+        return signalStrength
+    }
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
+
+    val input = readInput("Day10")
+    part1(input).println()
+    part2(input).println()
+}
